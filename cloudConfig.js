921386662947db8c8ef067042f2ac4814935f630
c++ Dsa <jauharking786@gmail.com>
2026-04-09 13:22:21 +0530

    final fix cloudinary

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,14 +1,14 @@
 const cloudinary = require("cloudinary").v2;
-const multerStorageCloudinary = require("multer-storage-cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-const CloudinaryStorage = multerStorageCloudinary.CloudinaryStorage;
-
+// 🔥 ENV se values lo
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
+// 🔥 STORAGE
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
